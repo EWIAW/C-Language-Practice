a857ac4115c3d22e7d3ba25de80c4ce647b4ebdb
EWIAW <zhongrongbin2021@163.com>
2023-10-26 12:56:40 +0800

    C语言练习

--- C语言练习-github/C语言练习-github/test.c
+++ C语言练习-github/C语言练习-github/test.c
@@ -1,7 +1,27 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+
 #include<stdio.h>
+
+int Two(int n)
+{
+	if (n > 1)//n要大于所求进制的小一位，如求二进制，则n>1，求六进制，则n>5
+	{
+		Two(n / 2);
+	}
+	printf("%d", n % 2);
+}
 
 int main()
 {
-	printf("hello world\n");
+	int input;
+	scanf("%d", &input);
+	Two(input);
 	return 0;
 }
